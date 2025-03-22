22c3755653ffaaa47e27d52b1279c68914e4adb2
Iradukunda Christian Tonny <122220713+irachrist1@users.noreply.github.com>
2025-03-22 13:17:24 +0200

    stack_3

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,6 +2,7 @@
 # Based on strace analysis, the issue is likely a misnamed file or typo
 
 exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/\.phpp/.php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin/', '/bin/'],
+  provider => 'shell',
 } 
